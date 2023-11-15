b986efe32e23a322b5cdb4e85a290bf35539919a
Joseph Cecil Odoi <88081064+Joseph-Cecil@users.noreply.github.com>
2023-11-15 04:42:26 +0000

    Update apiSlice.js

--- src/App/Api/apiSlice.js
+++ src/App/Api/apiSlice.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:5000"}),
+    baseQuery: fetchBaseQuery({baseUrl: "https://sprall-buy-api.onrender.com"}),
     tagTypes:['Order', 'User'],
     endpoints: builder => ({})
 })
