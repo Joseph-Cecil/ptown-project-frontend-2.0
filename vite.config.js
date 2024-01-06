15f1f8b4c66e727622bd44792309ffa6268838c3
Joseph-Cecil <josephgodwins015@gmail.com>
2024-01-06 05:37:49 +0000

    new update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 8080,
     proxy: {
       '/api': {
         target: 'https://ptbuy-api.onrender.com/',
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+      },
+    },
+  },
+});
